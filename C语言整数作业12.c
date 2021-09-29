cff38a4c5c70c99a2a1bc5d2cc308fd5ef47c669
GagaoY <47129484+GagaoY@users.noreply.github.com>
2021-09-29 16:40:55 +0800

    Add files via upload

---
+++ C语言整数作业12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+  srand(time(0)); // 播下随机种子
+
+  int result[52]; // 存放已生成的不重复的随机数
+  memset(result,0,sizeof(result));
+
+  int ii;
+
+  for (ii=0;ii<52;ii++)
+  {
+      int itmp=rand()%52+1;
+      printf("%d ",itmp)      
+  }
+
+  printf("\n");
+}
