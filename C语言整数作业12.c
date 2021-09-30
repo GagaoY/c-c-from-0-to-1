5a02252c1c863c6e053f2012eb64c5c7bf02c58a
GagaoY <47129484+GagaoY@users.noreply.github.com>
2021-09-30 20:53:22 +0800

    Update C语言整数作业12.c

--- C语言整数作业12.c
+++ C语言整数作业12.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <time.h>
 
-int main()
-{
   srand(time(0)); // 播下随机种子
 
   int result[52]; // 存放已生成的不重复的随机数
@@ -12,30 +10,32 @@ int main()
 
   int ii,jj;
 
-  int pos=0;
-  
-  for (ii=0;ii<52;ii++)
+  int pos=0; // 已生成随机数的个数
+
+  while (1)
   {
       int itmp=rand()%52+1;
-        
+
       // 除去重复数字
       for (jj=0;jj<52;jj++)
-        if (itmp==result[jj]) break;  // 找到了重复的数,就跳出循环，每次itmp生成的数跟result[jj]比较，
-    
-        if (jj<52) continue; //循环次数没够，继续找
-    
+      {
+      }
+        if (jj<52) continue; //如果jj<52,表示上面的循环是跳出的。没有执行完
+
       // 以下代码表示的是新的不重复的数
       printf ("%d ",itmp);
       result[pos]=itmp;  // 不同的数就存到result[pos]里
       pos++;
+
+      if (pos==52) break;
   }
 
     printf("\n");
-  
+
     for (ii=0;ii<52;ii++)
   {
-      printf("%d ",result[ii]);  
+      printf("%d ",result[ii]);
   }
-  
+
   printf("\n");
 }
